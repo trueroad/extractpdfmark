dccd699401ebe5211e948457739c1026799cf70a
Masamichi Hosoda <trueroad@trueroad.jp>
2016-10-08 13:29:30 +0900

    Add style for expected PDFmark tests

--- test/test-expected-pdfmark.sh
+++ test/test-expected-pdfmark.sh
@@ -1,19 +1,41 @@
 #!/bin/sh
 
 EXPECTEDPDFMARK=$1
-BASENAME=`basename ${EXPECTEDPDFMARK} | sed -e "s/-expected\.pdfmark$//"`
+BASENAME=`basename ${EXPECTEDPDFMARK} | sed -e "s/-expected\(-.\+\)\?\.pdfmark$//"`
+OPTSUFFIX=`echo ${EXPECTEDPDFMARK} | sed -e "s/^\(.*-expected\)\(-.\+\)\?\.pdfmark$/\2/"`
 
 PDF="${BASENAME}.pdf"
-EXTRACTEDPDFMARK="${BASENAME}-expected-test.pdfmark"
+EXTRACTEDPDFMARK="${BASENAME}-expected${OPTSUFFIX}-test.pdfmark"
 
 if [ ! -e ${EXPECTEDPDFMARK} ]; then
-    EXPEXTEDPDFMARK="${srcdir}/${BASENAME}-expected.pdfmark"
+    EXPEXTEDPDFMARK="${srcdir}/${BASENAME}-expected${OPTSUFFIX}.pdfmark"
 fi
 if [ ! -e ${PDF} ]; then
     PDF="${srcdir}/${BASENAME}.pdf"
 fi
 
-${top_builddir}/src/extractpdfmark ${PDF} | \
+case "$OPTSUFFIX" in
+    "-literal" )
+	OPTION="--style=literal"
+	;;
+    "-literal-escape" )
+	OPTION="--style=literal --escape"
+	;;
+    "-hex" )
+	OPTION="--style=hex"
+	;;
+    "-name" )
+	OPTION="--style=name"
+	;;
+    "-name-escape" )
+	OPTION="--style=name --escape"
+	;;
+    * )
+	OPTION=""
+	;;
+esac
+
+${top_builddir}/src/extractpdfmark ${OPTION} ${PDF} | \
     ${GREP} -v "^%\|^$" > ${EXTRACTEDPDFMARK}
 
 if test x"$DIFF" = x; then
