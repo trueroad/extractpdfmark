5156e9937deda933f3f36458502a46024848cd46
Masamichi Hosoda <trueroad@trueroad.jp>
2016-10-08 13:43:46 +0900

    Add style for round test

--- test/test-round.sh
+++ test/test-round.sh
@@ -1,15 +1,37 @@
 #!/bin/sh
 
 EXTRACTEDPDFMARK=$1
-BASENAME=`basename ${EXTRACTEDPDFMARK} | sed -e "s/-round\.ps$//"`
+BASENAME=`basename ${EXTRACTEDPDFMARK} | sed -e "s/-round\(-.\+\)\?\.ps$//"`
+OPTSUFFIX=`echo ${EXTRACTEDPDFMARK} | sed -e "s/^\(.*-round\)\(-.\+\)\?\.ps$/\2/"`
 
 PDF="${BASENAME}.pdf"
-PDF2="${BASENAME}-round-test.pdf"
-EXTRACTEDPDFMARK2="${BASENAME}-round-test.ps"
+PDF2="${BASENAME}-round${OPTSUFFIX}-test.pdf"
+EXTRACTEDPDFMARK2="${BASENAME}-round${OPTSUFFIX}-test.ps"
 
 if [ ! -e ${PDF} ]; then
     PDF="${srcdir}/${BASENAME}.pdf"
 fi
+
+case "$OPTSUFFIX" in
+    "-literal" )
+	OPTION="--style=literal"
+	;;
+    "-literal-escape" )
+	OPTION="--style=literal --escape"
+	;;
+    "-hex" )
+	OPTION="--style=hex"
+	;;
+    "-name" )
+	OPTION="--style=name"
+	;;
+    "-name-escape" )
+	OPTION="--style=name --escape"
+	;;
+    * )
+	OPTION=""
+	;;
+esac
 
 if test x"$GHOSTSCRIPT" = x; then
     # skip
@@ -18,7 +40,7 @@ fi
 
 ${GHOSTSCRIPT} -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -sOutputFile=${PDF2} \
     ${PDF} ${EXTRACTEDPDFMARK}
-${top_builddir}/src/extractpdfmark -o ${EXTRACTEDPDFMARK2} ${PDF2}
+${top_builddir}/src/extractpdfmark ${OPTION} -o ${EXTRACTEDPDFMARK2} ${PDF2}
 
 if test x"$DIFF" = x; then
     # skip
