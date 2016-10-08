2bd9923512bc669c915f7ab63e1f9792ed3899d4
Masamichi Hosoda <trueroad@trueroad.jp>
2016-10-08 20:48:59 +0900

    Tweak .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,10 +1,10 @@
 ;;; Directory Local Variables
 ;;; See Info node `(emacs) Directory Variables' for more information.
 
-((c++-mode
-  (c-default-style . "gnu")
-  (indent-tabs-mode))
- (autoconf-mode
-  (indent-tabs-mode))
- (shellscript-mode
-  (indent-tabs-mode)))
+((c++-mode .
+  ((c-default-style . "gnu")
+  (indent-tabs-mode . nil)))
+ (autoconf-mode .
+  ((indent-tabs-mode . nil)))
+ (shellscript-mode .
+  ((indent-tabs-mode . nil))))
