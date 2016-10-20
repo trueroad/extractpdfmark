4a1fcf04edeceb4ab8c049d2afb2c70d314d11c1
Masamichi Hosoda <trueroad@trueroad.jp>
2016-10-20 21:23:48 +0900

    Prepare iconv

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,37 @@
 #!/bin/sh
 
+CURRENT_DIR=`pwd`
+AUTOPOINT_TMP_DIR=`mktemp -d autopoint.tmp.XXXXXXXXXX`
+
+echo "*** Entering temporary directory ${AUTOPOINT_TMP_DIR}"
+cd ${AUTOPOINT_TMP_DIR}
+
+echo
+echo "*** Creating dummy configure.ac"
+{
+    cat <<EOF
+AM_GNU_GETTEXT_REQUIRE_VERSION([0.19.8])
+AM_GNU_GETTEXT([external])
+EOF
+} > configure.ac
+
+echo
+echo "*** Invoking autopoint"
+
+autopoint
+
+echo
+echo "*** Leaving temporary directory ${AUTOPOINT_TMP_DIR}"
+cd ${CURRENT_DIR}
+
+echo
+echo "*** Copying config.rpath from temporary directory"
+cp ${AUTOPOINT_TMP_DIR}/config.rpath .
+
+echo
+echo "*** Deleting temporary directory"
+rm -fr ${AUTOPOINT_TMP_DIR}
+
+echo
+echo "*** Invoking autoreconf"
 autoreconf -v -i
