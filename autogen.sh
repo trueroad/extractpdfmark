4837c1aca7e3f757acacf248e6e06eda36de7c33
Masamichi Hosoda <trueroad@trueroad.jp>
2016-11-27 21:57:28 +0900

    Add warning and error messges when autopoint is old
    Werner LEMBERG reported the following issue:
    compilation from git started with a call to `./autogen.sh' fails if an
    older version of gettext (e.g., 0.19.2) is used.
    
    This commit makes adding warning and error messages when autopoint is old.

--- autogen.sh
+++ autogen.sh
@@ -18,7 +18,13 @@ EOF
 echo
 echo "*** Invoking autopoint"
 
-autopoint
+autopoint ||
+    {
+	echo
+	echo "WARNING: autopoint failed."
+	echo "WARNING: It is required for getting \`config.rpath'."
+	echo "WARNING: Please consider to install autopoint-0.19.6+."
+    }
 
 echo
 echo "*** Leaving temporary directory ${AUTOPOINT_TMP_DIR}"
@@ -26,7 +32,20 @@ cd ${CURRENT_DIR}
 
 echo
 echo "*** Copying config.rpath from temporary directory"
-cp ${AUTOPOINT_TMP_DIR}/config.rpath .
+cp ${AUTOPOINT_TMP_DIR}/config.rpath . ||
+    {
+	echo
+	echo "WARNING: Copy failed."
+	if [ -e config.rpath ]; then
+	    echo "WARNING: Using existing \`config.rpath'."
+	else
+	    echo
+	    echo "ERROR: config.rpath does not exist."
+	    echo "ERROR: Please consider to install autopoint-0.19.6+"
+	    echo "ERROR: or copy \`config.rpath'."
+	    exit 1
+	fi
+    }
 
 echo
 echo "*** Deleting temporary directory"
