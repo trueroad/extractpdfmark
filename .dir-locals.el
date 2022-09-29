aaa2c508a2b7439564c7d92713cee2e691d2d761
Masamichi Hosoda <trueroad@trueroad.jp>
2022-09-29 23:14:08 +0900

    Fix .dir-local.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,10 +1,10 @@
 ;;; Directory Local Variables
 ;;; See Info node `(emacs) Directory Variables' for more information.
 
-((c++-mode .
-  ((c-default-style . "gnu")
+((c++-mode
+  (c-file-style . "gnu")
+  (indent-tabs-mode . nil))
+ (autoconf-mode
+  (indent-tabs-mode . nil))
+ (shellscript-mode
   (indent-tabs-mode . nil)))
- (autoconf-mode .
-  ((indent-tabs-mode . nil)))
- (shellscript-mode .
-  ((indent-tabs-mode . nil))))
